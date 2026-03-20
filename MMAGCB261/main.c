aab28acb58f448a99b76163dfc0f992444595127
jd161746 <caleb.christensen@my.jcu.edu.au>
2026-03-20 11:27:05 +1000

    TEST FOR GROUP WORK
    yes homie check this stuff

---
+++ MMAGCB261/main.c
@@ -0,0 +1,18 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 20-Mar-2026 Lawrence Christensen
+ * MMAGCB261
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+    printf("NO YOU LOSER\n");
+  }
+  return 0;
+}
